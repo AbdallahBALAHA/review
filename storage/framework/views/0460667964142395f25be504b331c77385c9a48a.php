59e1774e1e1dae37c7319e16bfc0ce80a00bc8e8
AbdallahBALAHA <89984755+AbdallahBALAHA@users.noreply.github.com>
2022-01-18 00:49:28 +0300

    Delete storage directory

--- storage/framework/views/0460667964142395f25be504b331c77385c9a48a.php
+++
@@ -1,8 +0,0 @@
-<style type="text/css">
-    form{
-        width: 460px;
-        margin: 20px auto;
-        padding: 20px;
-    }
-</style>
-<?php /**PATH P:\Downloads\AKB_Project-main\resources\views/includes/AppJS.blade.php ENDPATH**/ ?>
--- storage/framework/views/c1611f386128dedb09be11fceb2236d894581dac.php
+++
@@ -1,4 +0,0 @@
-<footer class="section">
-    <div class="center grey-text">Copyrights 2022 A.K. BALAHA</div>
-</footer>
-<?php /**PATH P:\Downloads\AKB_Project-main\resources\views/includes/footer.blade.php ENDPATH**/ ?>
